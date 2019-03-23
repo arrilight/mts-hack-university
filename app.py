c8fb04797061e0f2b9f93545a71975a24d0a63b2
Dmitry Grigorev <gridmi007@gmail.com>
2019-03-23 12:18:27 +0300

    service mockup

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 sessionStorage = {}
 
 
+@app.route('/', methods=['GET'])
+def test():
+    return 'lol'
+
+
 @app.route('/', methods=['POST'])
 def hello_world():
 
@@ -41,7 +46,8 @@ def handle_dialog(req, res):
             ]
         }
 
-        res['response']['text'] = 'Привет! Я твой новый помощник в мире МТС! Что ты хочешь сделать'
+        res['response']['text'] = 'Привет! Я твой новый помощник в мире МТС! Чтобы мы могли свободно общаться, ' \
+                                  'я должен знать что ты это ты. Для этого введи свой номер телефона)'
         res['response']['buttons'] = get_suggests(user_id)
         return
 
@@ -69,12 +75,12 @@ def get_suggests(user_id):
     # Выбираем две первые подсказки из массива.
     suggests = [
         {'title': suggest, 'hide': True}
-        for suggest in session['suggests'][:2]
+        for suggest in session['suggests']
     ]
 
     # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
-    session['suggests'] = session['suggests'][1:]
-    sessionStorage[user_id] = session
+    # session['suggests'] = session['suggests'][1:]
+    # sessionStorage[user_id] = session
 
     return suggests
 
---
+++ service.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+authorizedUses = set()
+userToSMS = {}
+
+
+@app.route('/sms_send', methods=['POST'])
+def send_sms():
+    # Вклюдчить генерацию возможно
+    userToSMS[request.json['session_id']] = ["я", "ем", "мороженное"]
+    return 'Sms was sent', 200
+
+
+@app.route('/sms_check', methods=['POST'])
+def check_sms():
+    if not request.json.__contains__('words') or userToSMS[request.json['session_id']] != request.json['words']:
+        return 'Words are not equal!', 401
+    else:
+        authorizedUses.add(request.json['session_id'])
+        return 'Successful', 200
+
+
+@app.route('/', methods=['POST'])
+def hello_world():
+    if not check_authorization(request):
+        return 'Not authorized', 401
+    else:
+        return 'Hello world!'
+
+
+def check_authorization(req):
+    return req.json.__contains__('session_id') and authorizedUses.__contains__(req.json['session_id'])
+
+
+if __name__ == "__main__":
+    context = ('cert.crt', 'key.key')
+    # app.run(host='0.0.0.0', port=80, ssl_context=context, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=8000)
