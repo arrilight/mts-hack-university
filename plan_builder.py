236318645f2ab280d000b48df8aafb208cfa18c3
nicholas <bul-nick-al@ya.ru>
2019-03-23 12:21:32 +0300

    plan builder added

--- plan_builder.py
+++ plan_builder.py
@@ -1,4 +1,15 @@
 import workflow
 
-flow = workflow.flow
 
+class PlanBuilder:
+    def __init__(self, saved_state=workflow.flow["build_plan"]):
+        self.flow = workflow.flow["build_plan"]
+        self.current_state = saved_state["init"]
+        self.title = saved_state["title"]
+        self.suggests = saved_state["suggests"]
+
+    def process_step(self, data):
+        result = {}
+        getattr(self, self.current_state)(data)
+        result["suggests"] = self.suggests
+        return result
